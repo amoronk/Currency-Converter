b7e3822fa10d7f2509260e144f939da48ef58117
amoronk <moronkeanifowose1@gmail.com>
2025-03-05 18:55:32 +0100

    Setup required res files

---
+++ app/src/main/java/com/amoronk/currencyconverter/presentation/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.amoronk.currencyconverter.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF185DFA)
+val PrimaryBlueDark = Color(0xFF012CEA)
+val PrimaryBlueDeep = Color(0xFF0021AF)
+
+val SecondaryGreen = Color(0xFF01B252)
+val SecondaryLightBlue = Color(0xFFA0C1E3)
+val SecondaryMediumBlue = Color(0xFF52A2C7)
+
+val BackgroundWhite = Color(0xFFFFFFFF)
+val BackgroundLightBlue = Color(0xFFF5F8FF)
+
+val TextPrimary = Color(0xFF000000)
+val TextSecondary = Color(0xFF757575)
+
+val GrayLight = Color(0xFFE0E0E0)
+val GrayMedium = Color(0xFFBDBDBD)
+val ErrorRed = Color(0xFFB00020)
+val DividerColor = Color(0xFFEEEEEE)
